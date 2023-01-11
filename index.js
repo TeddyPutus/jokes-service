4e451498dbe4e396ef9baa64eb7eb1de89868c5c
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2023-01-11 14:38:36 +0000

    one massive ternary that passes all the tests

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
 const { Op } = require("sequelize");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes/', async (req, res, next) => {
+app.get("/jokes/", async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    // {where:{"tags": {[Op.like]: '%' + req.query.tags ? req.query.tags : "" + '%'}}, {"content": {[Op.like]: '%' + req.query.content ? req.query.content : "" + '%'}}}
-    const jokes = await Joke.findAll(req.query.tags? Array.isArray(req.query.tags)? {where:{"tags":{ in: {[Op.like]: '%' + req.query.tags + '%'}}}} : {where:{"tags": {[Op.like]: '%' + req.query.tags + '%'}}} : {}, {attributes: {
-      exclude: ['createdAt', 'updatedAt']
-  }});
+    if (Array.isArray(req.query.tags)) console.log(req.query.tags);
+    const jokes = await Joke.findAll(
+      req.query.tags
+        ? Array.isArray(req.query.tags)
+          ? {
+              where: {
+                tags: {
+                  [Op.or]: req.query.tags.map((tag) => {
+                    return { [Op.like]: "%" + tag + "%" };
+                  }),
+                },
+                joke: req.query.content
+                  ? { [Op.like]: "%" + req.query.content + "%" }
+                  : { [Op.like]: "%" },
+              },
+            }
+          : {
+              where: {
+                tags: { [Op.like]: "%" + req.query.tags + "%" },
+                joke: req.query.content
+                  ? { [Op.like]: "%" + req.query.content + "%" }
+                  : { [Op.like]: "%" },
+              },
+            }
+        : {
+            where: {
+              joke: req.query.content
+                ? { [Op.like]: "%" + req.query.content + "%" }
+                : { [Op.like]: "%" },
+            },
+          },
+      {
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      }
+    );
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
